0cd0f24ea3d2e2a612ba3c900cb250abfc036fec
OlgaO2022 <112818191+OlgaO2022@users.noreply.github.com>
2022-12-17 18:58:13 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,7 @@
 #include <string>
 #include "Flora.h"
 
-
-
-
 int main() {
-
 		Flowers flower;
 		flower.type = "rosaceae";
 		flower.group_by_decorative = "blooming";
@@ -17,8 +13,6 @@ int main() {
 		flower.rose.roseship.number_of_petals = 5;
 		flower.rose.roseship.color = "red";
 
-
-		// можно проинициализировать так:
 		// Flowers flower{ "rosaceae", "blooming", "rose", 30.5, "rosehip", 5, "red" };
 
 		std::cout << "color - " << flower.rose.roseship.color << '\n';
