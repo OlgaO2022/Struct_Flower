360de0f9c413f212e1db048e43e2f910f0d033f0
OlgaO2022 <112818191+OlgaO2022@users.noreply.github.com>
2022-12-17 18:57:28 +0500

    Update Flora.h

--- Flora.h
+++ Flora.h
@@ -1,17 +1,17 @@
 #pragma once
 #include <string>;
 
-struct WildFlowers { // структура 3
+struct WildFlowers { // struct 3
 	std::string name;
 	int	number_of_petals;
 	std::string color;
 };
-struct Roses {  // структура 2
+struct Roses {  // struct 2
 	std::string family_of_flowers;
 	double flower_height;
 	WildFlowers roseship;
 };
-struct Flowers { // структура 1
+struct Flowers { // struct 1
 	std::string type;
 	std::string group_by_decorative;
 	Roses rose;
